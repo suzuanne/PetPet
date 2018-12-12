f7c6196b2578b7f8a7d2d70dcee4bd85d9bec137
satomi-kimura <satomi-k@nttr.co.jp>
2018-12-12 15:37:32 +0900

    Initial Commit

---
+++ PetPet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PetPet
+//
+//  Created by nttr on 2018/12/12.
+//  Copyright © 2018年 nttr.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
