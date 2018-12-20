fa54e0e69e778ad0d99b8f3e17a761ee7c823c38
satomi-kimura <satomi-k@nttr.co.jp>
2018-12-20 14:54:38 +0900

    ログインつける前！

---
+++ PetPet/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  PetPet
+//
+//  Created by nttr on 2018/12/20.
+//  Copyright © 2018年 nttr.inc. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+
+  
+}
