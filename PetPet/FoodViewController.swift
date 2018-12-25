47cba0bc49f090dfb8792148cc31e7e85f96d777
satomi-kimura <satomi-k@nttr.co.jp>
2018-12-25 17:17:49 +0900

    ・今日の日付を表示 ・ニフティクラウドにボタンを押した時間を保存、同じボタンで読み込み

--- PetPet/FoodViewController.swift
+++ PetPet/FoodViewController.swift
@@ -7,17 +7,81 @@
 //
 
 import UIKit
+import NCMB
 
 class FoodViewController: UIViewController {
-
+    
+    @IBOutlet var labelClock: UILabel!
+    @IBOutlet var labelDate: UILabel!
+    var displayTime: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+      labelClock.text = "まだあげていないようです"
+        
+        func getToday(format:String = "yyyy/MM/dd HH:mm:ss") -> String {
+            let now = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = format
+            return formatter.string(from: now as Date)
+        }
+        labelDate.text = getToday(format:"yyyy-MM-dd")
+       
+
         
     }
     
-
-
-
+    
+    // 現在時刻を表示する処理
+    @IBAction func displayClock() {
+        // 現在時刻を「HH:MM:SS」形式で取得する
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd HH:mm"
+        var displayTime = formatter.string(from: Date())    // Date()だけで現在時刻を表す
+        
+        // 0から始まる時刻の場合は「 H:MM:SS」形式にする
+        if displayTime.hasPrefix("0") {
+            // 最初に見つかった0だけ削除(スペース埋め)される
+            if let range = displayTime.range(of: "0") {
+                displayTime.replaceSubrange(range, with: " ")
+            }
+        }
+        
+        // ラベルに表示
+        labelClock.text = displayTime
+        
+    }
+    
+    @IBAction func save(){
+        let object = NCMBObject(className: "food")
+        
+        object?.setObject(labelClock.text, forKey: "text")
+        object?.saveInBackground({ (error) in
+            if error != nil {
+                print("error")
+            }else {
+                print("success")
+            }
+        })
+    }
+    
+    @IBAction func loadData(){
+        let query = NCMBQuery(className: "food")
+        query?.findObjectsInBackground({ (result, error) in
+            if error != nil {
+                print("error")
+            } else {
+                let messages = result as! [NCMBObject]
+                let text = messages.last?.object(forKey: "text") as! String
+                
+                self.labelClock.text = text
+            }
+        })
+    }
+    
+    
 }
+
+
+
--- PetPet/LoginViewController.swift
+++ PetPet/LoginViewController.swift
@@ -7,15 +7,50 @@
 //
 
 import UIKit
+import NCMB
 
 class LoginViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let uuid = UIDevice.current.identifierForVendor?.uuidString
+        NCMBUser.logInWithUsername(inBackground: uuid, password: uuid) { (user, login_error) in //★1
+            if login_error != nil {
+                // ログイン失敗時の処理
+                let login_err : NSError = login_error as! NSError
+                print("ログインに失敗しました。エラーコード：\(login_err.code)")
+                // 初回利用（ユーザー未登録）の場合
+                if login_err.code == 401002 { // 401002：ID/Pass認証エラー ★2
+                    /* ★3 */
+                    /* mBaaSユーザー登録 */
+                    let new_user = NCMBUser()
+                    new_user.userName = uuid
+                    new_user.password = uuid
+                    new_user.signUpInBackground({ (signUp_error) in
+                        if signUp_error != nil {
+                            // ユーザー登録失敗時の処理
+                            
+                        } else {
+                            // ユーザー登録成功時の処理
+                            
+                        }
+                    })
+                }
+                
+            } else {
+                // ログイン成功時の処理
+                
+            }
+        }
+    }
+  
 
     }
     
 
+
   
-}
+
