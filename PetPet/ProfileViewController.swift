3b08ded69359b3fe890494dc3de78c1188bb3a11
satomi-kimura <satomi-k@nttr.co.jp>
2018-12-27 16:36:35 +0900

    ばぐってるとき（アルバムから選択つけたあと）

---
+++ PetPet/ProfileViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ProfileViewController.swift
+//  PetPet
+//
+//  Created by nttr on 2018/12/26.
+//  Copyright © 2018年 nttr.inc. All rights reserved.
+//
+
+import UIKit
+import NCMB
+
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    // 写真を表示するビュー
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = UIImage(named: "default.png")
+        
+    }
+    
+    // カメラロールから写真を選択する処理
+    @IBAction func choosePicture() {
+        // カメラロールが利用可能か？
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            // 写真を選ぶビュー
+            let pickerView = UIImagePickerController()
+            // 写真の選択元をカメラロールにする
+            // 「.camera」にすればカメラを起動できる
+            pickerView.sourceType = .photoLibrary
+            // デリゲート
+            pickerView.delegate = self
+            // ビューに表示
+            self.present(pickerView, animated: true)
+        }
+    }
+ 
+    // 写真を選んだ後に呼ばれる処理
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        // 選択した写真を取得する
+        let image = info[.originalImage] as! UIImage
+        // ビューに表示する
+        self.imageView.image = image
+        // 写真を選ぶビューを引っ込める
+        self.dismiss(animated: true)
+    }
+    
+    // 写真をリセットする処理
+    @IBAction func resetPicture() {
+        // アラートで確認
+        let alert = UIAlertController(title: "確認", message: "画像を初期化してもよいですか？", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler:{(action: UIAlertAction) -> Void in
+            // デフォルトの画像を表示する
+            self.imageView.image = UIImage(named: "default.png")
+        })
+        let cancelButton = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        // アラートにボタン追加
+        alert.addAction(okButton)
+        alert.addAction(cancelButton)
+        // アラート表示
+        present(alert, animated: true, completion: nil)
+    }
+}
